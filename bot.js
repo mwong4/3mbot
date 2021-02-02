1b78947e320854b988d53904f57f87406f5f37bd
Max <wong.max.c@gmail.com>
2021-02-01 21:49:25 -0500

    Initial settup

---
+++ bot.js
@@ -0,0 +1,55 @@
+/*
+Example code from this source: https://www.digitaltrends.com/gaming/how-to-make-a-discord-bot/
+
+Adapted by Max Wong for 3mber
+*/
+
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+var promptChar = '>'; //This is the char used to prompt commands
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with prompt given
+    if (message.substring(0, 1) == promptChar) {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            case 'intro':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Hello World! Welcome all'
+                });
+            break;
+
+            // Just add any case commands if you want to..
+         }
+     }
+});
