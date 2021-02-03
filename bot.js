e7a3d84758b5bdfb0a577296ceba144498e57520
Max <wong.max.c@gmail.com>
2021-02-03 10:09:02 -0500

    Finished implementing the basiec sequelize database system

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ const sequelize = new Sequelize('database', 'user', 'password', {
  * usage INT
  * );
 */
-const tags = sequelizee.define('tags', {
+const Tags = sequelize.define('tags', {
 	name: {
 		type: Sequelize.STRING,
 		unique: true,
@@ -51,7 +51,7 @@ const tags = sequelizee.define('tags', {
 
 client.once('ready', () => {
 	// [gamma] Syncing the model
-	tags.sync({ force:true }); //{force:true} is for debugging
+	Tags.sync({ force:true }); //{force:true} is for debugging
 });
 
 client.on('message', async message => {
@@ -61,17 +61,74 @@ client.on('message', async message => {
 		const commandArgs = input.join(' ');
 
 		if (command === 'addtag') {
-			// [delta]
+			// [delta] Adding a tag
+			const splitArgs = commandArgs.split(' ');
+			const tagName = splitArgs.shift();
+			const tagDescription = splitArgs.shift(' ');
+
+			try {
+				// equivalent to: INSET INTO tags (name, desciprion, username) values (?, ?, ?);
+				const tag = await Tags.create({
+					name: tagName,
+					description: tagDesciprtion,
+					username: message.author.username,
+				});
+				return message.reply(`tag ${tag.name} added.`);
+			}
+			catch (e) {
+				if (e.name === 'SequelizeUniqueConstraintError') {
+					return message.reply('That tag already exists.');
+				}
+				return message.reply('Something went wrong with adding a tag.');
+			}
 		} else if (command === 'tag') {
-			// [epsilon]
+			// [epsilon] Fetching a tag
+			const tagName = commandArgs;
+
+			//Equivalent to: SELECT * FROm tags WHERE name = 'tagName' LIMIT 1;
+			const tag = await Tags.findOne({ where: { name: tagName} });
+			if (tag) {
+				//Equivalent to: UPDATE tags SET usage_count = usage_count + 1 WHERE name = 'tagName';
+				tag.increment('usage_count');
+				return message.channel.send(tag.get('description'));
+			}
+			return message.reply(`Could not find tag: ${tagName}`);
 		} else if (command === 'edittag') {
-			// [zeta]
+			// [zeta] Editing a tag
+			const splitArgs = commandArgs.split(' ');
+			const tagName = splitArgs.shift();
+			const tagDescription = splitArgs.join(' ');
+
+			//Equivalent to: UPDATE tags (description) values (?) WHERE name='?';
+			const affectedRows = await Tags.update({description: tagDescription}, {where: {name: tagName} });
+			if(affectedRows > 0) {
+				return message.reply(`Tag ${tagName} was edited.`);
+			}
+			return message.reply(`Could not find a tag with name ${tagName}.`);
 		} else if (command === 'taginfo') {
-			// [theta]
+			// [theta] Dsplay info on a specific tag
+			const tagName = commandArgs;
+
+			//Equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
+			const tag = await Tags.findOne({ where: {name: tagName} });
+			if(tag) {
+				return message.channel.send(`${tageName} was created by ${tag.username} at ${tag.createdAt} and has been used ${tag.usage_count} times.`)
+			}
+			return message.reply(`Could not find tag: ${tagName}`);
 		} else if (command === 'showtags') {
-			// [lambda]
+			// [lambda] Listing all tags
+			//Equivalent to: SELECT name FROm tags;
+			const tagList = await Tags.findAll({attribute: ['name']});
+			const tagString = tagList.map(t => t.name).join(', ') || 'No tags set.';
+			return message.channel.send(`List of tags: ${tagString}`);
 		} else if (command === 'removetag') {
-			// [mu]
+			// [mu] Deleting a tag
+			const tagName = commandArgs;
+			//Equivallent to: DELETE from tags WHERE name = ?;
+			const rowCount = await Tags.destroy({where: {name: tagName} });
+			if (!rowCount) return message.reply('That tag did not exist.');
+
+			return message.reply('Tag deleted');
 		}
 	}
 });
