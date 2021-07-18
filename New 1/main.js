5fab655ffd399b22a6a844d7592a67834b17ee56
Max <wong.max.c@gmail.com>
2021-07-18 17:54:20 -0400

    Set up basic database connection

--- New 1/main.js
+++ New 1/main.js
@@ -10,6 +10,8 @@ const fs = require('fs'); //Allow to acces other js files
 const Discord = require('discord.js');
 require('dotenv').config(); //Requiring .env file
 
+const mongoose = require("mongoose");
+
 const client = new Discord.Client();
 
 client.commands = new Discord.Collection(); //command and event collections
@@ -18,7 +20,18 @@ client.events = new Discord.Collection();
 //Execute command and event handlers
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
-})
+});
+
+//Connect to database through mongoose package
+mongoose.connect(process.env.MONGODB_SRV, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(()=>{
+    console.log('3mbot Database Connected'); //let us know connection
+}).catch((err) => {
+    console.log(err); //let us know error
+});
 
 
 client.login(process.env.DISCORD_TOKEN); //Login token
