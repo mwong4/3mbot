8b0a09c79ef4bd02515c3433b862a7121970c161
Max <wong.max.c@gmail.com>
2021-07-18 10:13:09 -0400

    Finished experimenting with basic permission code

--- New 1/commands/test.js
+++ New 1/commands/test.js
@@ -12,14 +12,43 @@ module.exports =
     description: "test permissions",
     execute(client, message, args)
     {
+        let role = message.guild.roles.cache.find(r => r.name === "Mod");
+
+        //Checking by id
         if(message.member.roles.cache.has('866306383658221628')) //If member has correct credentials
         {
             message.channel.send('https://www.youtube.com/watch?v=Qik6A7UAGfk'); //Send 'ping' in channel
         }
         else
         {
-            message.channel.send('ERROR: Mod Permissions Required'); //Send 'ping' in channel
+            message.channel.send('ERROR: Mod Permissions Required.'); //Send 'ping' in channel
+            //message.member.roles.add('866306383658221628').catch(console.error); //add, remove
         }
         
+        /*
+        //Checking by name instead of id
+
+        if(message.member.roles.cache.some(r => r.name === "Mod")) //If member has correct role name
+        {
+            message.channel.send('https://www.youtube.com/watch?v=Qik6A7UAGfk'); //Send 'ping' in channel
+        }
+        else
+        {
+            message.channel.send('ERROR: Mod Permissions Required. Adding to Mod...'); //Send 'ping' in channel
+            message.member.roles.add(role).catch(console.error);
+        }
+        */
+
+        /*
+        //Checking specific permissions
+        if(message.member.permissions.has("KICK_MEMBERS"))
+        {
+            message.channel.send('You have permission to kick members');
+        }
+        else
+        {
+            message.channel.send('You DONT have permission to kick');
+        }
+        */
     },
 };
