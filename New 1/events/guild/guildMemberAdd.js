d24f9bbd559cc35cc8606f4e2140bd40478a54d7
Max <wong.max.c@gmail.com>
2021-07-19 20:15:22 -0400

    Trying to get profile creation working

---
+++ New 1/events/guild/guildMemberAdd.js
@@ -0,0 +1,20 @@
+/*
+Author: Iamwaxy
+Date Created: July 19, 2021
+Purpose: Used to add users
+
+Follow Tutorials: CodeLyon
+*/
+
+const profileModel = require('../../models/profileSchema'); //get model
+
+module.exports = async(client, discord, member) =>{
+    
+    let profile = await profileModel.create({ //make profile
+        userID: member.id,
+        serverID: member.guild.id,
+        coins: 1000,
+        bank: 0
+    });
+    profile.save(); //Save profile to database  
+};
--- New 1/events/guild/message.js
+++ New 1/events/guild/message.js
@@ -1,15 +1,40 @@
 /*
 Author: Iamwaxy
 Date Created: July 17, 2021
-Purpose: Runs on start
+Purpose: Handle messages from user to bot
 
 Follow Tutorials: CodeLyon
 */
 
-module.exports = (Discord, client, message) => {
+require("dotenv").config();
+const profileModel = require('../../models/profileSchema'); //get schema+model
+
+module.exports = async(Discord, client, message) => {
     const prefix = process.env.PREFIX; //initialize prefix from .env
 
     if(!message.content.startsWith(prefix) || message.author.bot) return; //filter out non prefix or bot messages
+
+    let profileData; //stores result of profile search
+    try
+    {
+        profileData = await profileModel.findOne({ userID: message.author.id });
+
+        if(!profileData) //if profile does not exist
+        {
+            console.log("Hi");
+            let profile = await profileModel.create({ //make profile
+                userID: message.author.id,
+                serverID: message.guild.id,
+                coins: 1000,
+                bank: 0
+            });
+            profile.save(); //Save profile to database
+        }
+    }
+    catch(err) //catch and output errors
+    {
+        console.log(err);
+    }
 
     const args = message.content.slice(prefix.length).split(/ +/); //Splicing command
     const cmd = args.shift().toLowerCase();
