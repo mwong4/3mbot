91d5fb307c1969347f8929840e95ee4f7974723d
Max <wong.max.c@gmail.com>
2021-07-19 21:28:19 -0400

    Added embeds

---
+++ New 1/commands/embedTest.js
@@ -0,0 +1,31 @@
+/*
+Author: Iamwaxy
+Date Created: July 19, 2021
+Purpose: Learning to do embeds
+
+Follow Tutorials: CodeLyon
+*/
+
+//const Discord = require('discord.js');
+
+module.exports = {
+    name: 'embedtest',
+    description: "Learning embeds",
+    execute(client, message, args, Discord) 
+    {
+        const newEmbed = new Discord.MessageEmbed()
+            .setTitle('Hi there')
+            .setColor('#d99307')
+            .setURL('https://www.youtube.com/watch?v=Qik6A7UAGfk')
+            .setDescription('Hey look, a new video!')
+            .addFields(
+                {name: 'Check out', value: 'Bedwards Grind Animation'},
+                {name: 'and', value: 'City Pigeon Demo'}
+            )
+            .attachFiles(['../Images/kerdTheBird.png'])
+            .setImage('attachment://kerdTheBird.png')
+            .setFooter("It's Kerd the Bird");
+
+        message.channel.send(newEmbed); //Send Image
+    }
+};
