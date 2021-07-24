bec6bdb30092645c60c8375d8db20efe09281da3
Max <wong.max.c@gmail.com>
2021-07-23 21:38:15 -0400

    Implemented ability to beg money

---
+++ 3mbot 1.2/commands/beg.js
@@ -0,0 +1,32 @@
+/*
+Author: Iamwaxy
+Date Created: July 23, 2021
+Purpose: For begging for coins
+
+Follow Tutorials: CodeLyon
+*/
+
+const profileModel = require('../models/profileSchema');
+
+module.exports = 
+{
+    name: 'beg',
+    aliases: [],
+    permissions: [],
+    description: "To beg for coins",
+    async execute(client, message, args, discord, profileData)
+    {
+        const randomNumber = Math.floor(Math.random() * 500) + 1; //generate random amount between 1 and 500
+        const response = await profileModel.findOneAndUpdate(
+        {
+            userID: message.author.id,
+        }, 
+        {
+            $inc: {
+                coins: randomNumber,
+            },
+        }
+        );
+        return message.channel.send(`${message.author.username}, you recieved ${randomNumber} coins`)
+    },
+};
