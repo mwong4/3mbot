2339c9f547fa8ad54e016434ee67ae0f4e4aa28d
Max <wong.max.c@gmail.com>
2021-08-03 15:04:42 -0400

    Edit item is finished

--- 3mbot 1.2/commands/edit.js
+++ 3mbot 1.2/commands/edit.js
@@ -11,9 +11,140 @@ module.exports =
     name: 'edit',
     aliases: ["edititem"],
     permissions: ["ADMINISTRATOR"],
-    description: "To edit an item. Syntax: >edit itemName",
+    description: "To edit an item. Syntax: >edit itemName  operation (name, description, numbersold, averagevalue, tradeable, tier, objtype)  value (value for selected operation)",
     async execute(client, message, args, Discord, profileData)
     {
-        
+        if(args.length < 3) return message.channel.send("ERROR: Missing arguments"); //Make sure enough arguments provided
+
+        const objName = args[0];
+        const operation = args[1];
+        const input  = args.splice(2, args.length).join(" ");
+
+        try
+        {
+            const targetData = await itemModel.findOne({name: args[0]}); //find target in database
+
+            if(targetData) //check if object exists
+            {
+                if(operation === "name")
+                {
+                    const response = await itemModel.findOneAndUpdate( //Set name
+                    {
+                        name: objName,
+                    }, 
+                    {
+                        $set: {
+                            name: input,
+                        },
+                    }
+                    );
+                }
+                else if(operation === "description")
+                {
+                    const response = await itemModel.findOneAndUpdate( //Set name
+                    {
+                        name: objName,
+                    }, 
+                    {
+                        $set: {
+                            description: input,
+                        },
+                    }
+                    );
+                }
+                else if(operation === "numbersold")
+                {
+                    //check positive whole number
+                    if(input % 1 != 0 || input < 0) return message.channel.send('ERROR: Amount must be a positive whole number');
+
+                    const response = await itemModel.findOneAndUpdate( //Set name
+                    {
+                        name: objName,
+                    }, 
+                    {
+                        $set: {
+                            numberSold: input,
+                        },
+                    }
+                    );
+                    
+                }
+                else if(operation === "averagevalue")
+                {
+                    //check positive whole number
+                    if(input % 1 != 0 || input < 0) return message.channel.send('ERROR: Amount must be a positive whole number');
+
+                    const response = await itemModel.findOneAndUpdate( //Set name
+                    {
+                        name: objName,
+                    }, 
+                    {
+                        $set: {
+                            averageValue: input,
+                        },
+                    }
+                    );
+                }
+                else if(operation === "tradeable")
+                {
+                    //check bool
+                    var convertedInput = false;
+
+                    if(input === "true") convertedInput = true;
+                    else if(input === "false") convertedInput = false;
+                    else return message.channel.send("ERROR: [tradeable] is not a boolean (ie, true, false)");
+
+                    const response = await itemModel.findOneAndUpdate( //Set name
+                    {
+                        name: objName,
+                    }, 
+                    {
+                        $set: {
+                            tradeable: convertedInput,
+                        },
+                    }
+                    );
+                }
+                else if(operation === "tier")
+                {
+                    const response = await itemModel.findOneAndUpdate( //Set name
+                    {
+                        name: objName,
+                    }, 
+                    {
+                        $set: {
+                            tier: input,
+                        },
+                    }
+                    );
+                }
+                else if(operation === "objtype")
+                {
+                    const response = await itemModel.findOneAndUpdate( //Set name
+                    {
+                        name: objName,
+                    }, 
+                    {
+                        $set: {
+                            objType: input,
+                        },
+                    }
+                    );
+                }
+                else
+                {
+                    return message.channel.send(`${operation} is not a valid operator. (ie. name, description, numbersold, averagevalue, tradeable, tier, objtype)`)
+                }
+                return message.channel.send(`${operation} on ${objName} has been updated`); //Send success message
+            }
+            else
+            {
+                return message.channel.send("ERROR: Object does not exist")
+            }
+        }
+        catch(err) //catch and output errors
+        {
+            console.log(err);
+        }
     },
 };
